718f4a13d278fbbf72b8519d23ac935ca8150d2a
Erayİnci <erayiinci@gmail.com>
2024-10-26 11:15:00 +0300

    +

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(shouldWakeUp(true,-1));
+        System.out.println("******************************");
         System.out.println(hasTeen(22,23,34));
+        System.out.println("******************************");
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println("******************************");
+
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
@@ -26,19 +31,31 @@ public class Main {
         return false;
     }
 
-//    public static boolean isCatPlaying(boolean isSummer, int temp) {
-//
-//    }
-//
-//    public static double area(double width, double height) {
-//
-//    }
-//
-//    public static double area(double radius) {
-//
-//    }
+    public static boolean isCatPlaying(boolean isSummer, int temp) {
+        if(isSummer){
+            if(temp >= 25 && temp <= 45){
+                return true;
+            }
+        }else{
+            if(temp>=25 && temp <=35){
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public static double area(double width, double height) {
+        if(width <  0 || height <0){
+            return -1;
+        }
+        return width*height;
+    }
 
+    public static double area(double radius) {
+        if(radius <0)
+            return -1;
+        return radius * radius * Math.PI;
+    }
 
 }
 
